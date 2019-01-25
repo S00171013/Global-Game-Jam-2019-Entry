ff9cc713daad3d3aa683a4cfc698bf40895246d9
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-01-25 21:31:46 +0000

    Main menu UI's finished.

---
+++ GGJ-MaestroJackG/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject quitDialogBox;
+
+    public enum menuState
+    {
+        MAIN,
+        QUIT
+    }
+
+    public menuState phase;
+
+	void Start () {
+        phase = menuState.MAIN;
+    }    
+
+    public void OpenQuitDialog() {
+        if (phase == menuState.MAIN) {
+            phase = menuState.QUIT;
+            quitDialogBox.SetActive(true);         
+        }
+    }
+
+    public void StartGame() {
+        if (phase == menuState.MAIN) SceneManager.LoadScene("sc_level_test");
+    }    
+
+    public void CloseQuitDialog() {
+        if (phase == menuState.QUIT) {
+            phase = menuState.MAIN;
+            quitDialogBox.SetActive(false);          
+        }
+    }
+
+    public void QuitGame() {
+        if (phase == menuState.QUIT) {        
+            Application.Quit(); // Close the game.
+        }
+    }
+}
