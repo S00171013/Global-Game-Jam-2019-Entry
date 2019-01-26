5b66246d5d8e63d36653ca5f8939f13904e2f827
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-01-26 13:01:44 +0000

    Rudimentary animations implemented.
    They'll need some tweaking down the line.

--- GGJ-MaestroJackG/Assets/Scripts/Player/PlayerController.cs
+++ GGJ-MaestroJackG/Assets/Scripts/Player/PlayerController.cs
@@ -6,36 +6,48 @@ public class PlayerController : MonoBehaviour {
 
     public int moveSpeed, jumpCount, jumpLimit;
     public float jumpForce;
+
+    bool moving, airborne;
     
     Rigidbody2D playerBody;
+    Animator animator;
  
     void Start () {      
-        playerBody = GetComponent<Rigidbody2D>();      
+        playerBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 	
 	void Update () {
+        animator.SetBool("Moving", moving); // Can this be moved to the Start() method? Worth testing later.
+        animator.SetInteger("JumpCount", jumpCount);
+        animator.SetBool("Airborne", airborne);
         HandleMovement();
 	}
 
-    void HandleMovement() {               
+    void HandleMovement() {
         // Left
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+            if (!moving) moving = true;
         }
 
         // Right
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+            if (!moving) moving = true;
         }
+
+        else if (moving) moving = false;
 
         // Jump.
         if (Input.GetKeyDown(KeyCode.W) && jumpCount < jumpLimit)
         {
             playerBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             jumpCount++;
-        }
+            airborne = true;
+        }       
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -43,6 +55,7 @@ public class PlayerController : MonoBehaviour {
         if(collision.gameObject.tag == "Ground")
         {
             jumpCount = 0;
+            airborne = false;
         }
     }
 }
